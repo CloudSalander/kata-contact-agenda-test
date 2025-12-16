f5145983d92d1cde0432dda2b5ac17c6426e74b6
CloudSalander <ruben.as532@gmail.com>
2025-12-16 19:52:43 +0100

    Now contact only accept value objects as creation parameters

--- src/classes/Contact.php
+++ src/classes/Contact.php
@@ -1,58 +1,18 @@
 <?php 
 
+include('value-objects/Name.php');
+include('value-objects/Surname.php');
 include('value-objects/Email.php');
 include('value-objects/PhoneNumber.php');
 
 class Contact {
-    //todo: EMAIL AND PHONE validations
-    const INPUT_NAME = "Enter name";
-    const INPUT_SURNAME = "Enter surname";
-    const INPUT_EMAIL = "Enter email";
-    const INPUT_PHONE = "Enter phone number";
-    const INPUT_EMAIL_ERROR = "Please, enter valid email";
-    const INPUT_PHONE_ERROR = "Please, enter valid phone number";
 
-    private string $name;
-    private string $surname;
-    private string $email;
-    private string $phone;
-
-    public function __construct() {
-        $this->name = readline(self::INPUT_NAME);
-        $this->surname = readline(self::INPUT_SURNAME);
-        $this->enterPhone();
-        $this->enterEmail();
-    }
-
-    public function getSurname(): string {
-        return $this->surname;
-    }
-
-    private function enterEmail(): void {
-        while(true) {
-            $email = readline(self::INPUT_EMAIL);
-            try{
-                $this->email = new Email($email);
-                break;
-            }
-            catch(InvalidArgumentException $e) {
-               echo self::INPUT_EMAIL_ERROR.PHP_EOL;
-            }
-        }
-    }
-
-    private function enterPhone(): void {
-        while(true) {
-            $phone = readline(self::INPUT_PHONE);
-            try{
-                $this->phone = new PhoneNumber($phone);
-                break;
-            }
-            catch(InvalidArgumentException $e) {
-               echo self::INPUT_PHONE_ERROR.PHP_EOL;
-            }
-        }
-    }
+    public function __construct(
+        private Name $name,
+        private Surname $surname,
+        private Email $email,
+        private PhoneNumber $phone
+    ) {}
 
     public function __toString() {
         return $this->surname.",".$this->name."  📞 ".$this->phone." ✉️  ".$this->email.PHP_EOL;
--- src/index.php
+++ src/index.php
@@ -5,13 +5,6 @@ include('classes/Agenda.php');
 define('EXIT_CODE',5);
 define('INPUT_MSG', "Please, enter your option(0 to 5)");
 
-include('classes/value-objects/Name.php');
-include('classes/value-objects/Surname.php');
-//$name = new Name("");
-//$surname = new Surname("");
-$name = new Name("Pepe");
-$surname = new Surname("Luis");
-/*
 $agenda = new Agenda();
 $menu = new Menu($agenda);
 
@@ -22,5 +15,5 @@ while($option != EXIT_CODE) {
     $option = readline(INPUT_MSG);
     $menu->doOption(intval($option));
 }
-*/
+
 ?>
